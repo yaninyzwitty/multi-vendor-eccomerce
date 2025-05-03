15798a60e2413a6cbf50aab81ca12f062ee879aa
yaninyzwitty <ianmwa143@gmail.com>
2025-05-03 16:56:47 +0300

    initial commit, #1

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import {Button} from "@/components/ui/button";
+import {Checkbox} from "@/components/ui/checkbox";
+import {Input} from "@/components/ui/input";
+import {Progress} from "@/components/ui/progress";
+import {Textarea} from "@/components/ui/textarea";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <div className="flex flex-col  gap-y-4">
+        <div>
+          <Button variant={"elevated"}>Hello witty</Button>
+        </div>
+        <div>
+          <Input placeholder="i AM an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I am textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
+    </div>
+  );
+}
