5dcf0644c78de6d9cd3bc735126fe09124f9a8c4
yaninyzwitty <ianmwa143@gmail.com>
2025-05-04 18:11:04 +0300

    add home layout

---
+++ app/(home)/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div>
+        <p>Funroad, Inc.</p>
+      </div>
+    </footer>
+  );
+}
--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -16,7 +16,7 @@ function Progress({
       className={cn(
         "bg-primary/20 relative h-3 w-full overflow-hidden rounded-full",
         // modified classes
-        "border bg-transparent ",
+        "border bg-white ",
         className
       )}
       {...props}
